b9d6cd8d5adada1f320b7ed5df26080e5f09447e
Your Name <you@example.com>
2019-12-06 23:37:02 +0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import TodoItem from './components/todoItem'
 class App extends Component {
   constructor(){
     super();
-    this.TodoList = ["Đi Chơi", "Đi Ngủ", "Xem Phim"];
+    this.TodoList = [
+      { title: "Đi Chơi", isComplete: true }, 
+      { title: "Đi Ngủ", isComplete: false }, 
+      { title: "Đi Xem Phim", isComplete: false }  
+    ];
   }
   render() {
     return (
       <div className="App">
         {
-          this.TodoList.map((item, index) => <TodoItem title={ item } key={ index } />)
+          this.TodoList.length > 0 && this.TodoList.map((item, index) => <TodoItem item = { item } key = { index } />)
+        }
+        {
+          this.TodoList.length === 0 && 'Nothing Item'
         }
       </div>
     );
