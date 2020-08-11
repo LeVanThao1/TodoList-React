63d40ec6835978e10b57e893929e9c0682788934
Your Name <you@example.com>
2020-08-11 11:13:57 +0700

    Updates

---
+++ precache-manifest.a71e0264687027c31e0d12958594ca98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79184a9df55928f22672d80121162cd",
+    "url": "/levanthao1.github.io/TodoList-React/index.html"
+  },
+  {
+    "revision": "155f1b6498e5658e0c74",
+    "url": "/levanthao1.github.io/TodoList-React/static/css/main.09879768.chunk.css"
+  },
+  {
+    "revision": "1354dfc7d15385565d3a",
+    "url": "/levanthao1.github.io/TodoList-React/static/js/2.b2649fd7.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/levanthao1.github.io/TodoList-React/static/js/2.b2649fd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "155f1b6498e5658e0c74",
+    "url": "/levanthao1.github.io/TodoList-React/static/js/main.58ecb4e6.chunk.js"
+  },
+  {
+    "revision": "156ab47eb0b2372152ed",
+    "url": "/levanthao1.github.io/TodoList-React/static/js/runtime-main.627898b6.js"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/levanthao1.github.io/TodoList-React/static/media/success.d29f1779.svg"
+  },
+  {
+    "revision": "0e561df147ed496d0c19349b0cceceb6",
+    "url": "/levanthao1.github.io/TodoList-React/static/media/tick-sign.0e561df1.svg"
+  },
+  {
+    "revision": "7a0423bd6098151cfe14f7d230cff8c5",
+    "url": "/levanthao1.github.io/TodoList-React/static/media/verified.7a0423bd.svg"
+  }
+]);
