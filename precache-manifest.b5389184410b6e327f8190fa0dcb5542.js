886cd993f7a203a7c2296903049af273ec7c7eed
Lê Văn Thảo <thaolv210402@gmail.com>
2021-05-20 10:18:48 +0700

    Updates

---
+++ precache-manifest.b5389184410b6e327f8190fa0dcb5542.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2827002a9bbc4f3a6d3f0140dee24e03",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "6ffb368cd0eea7b418c0",
+    "url": "/todoapp/static/css/main.75bd5093.chunk.css"
+  },
+  {
+    "revision": "4b0ffd68a3f6a087cbfe",
+    "url": "/todoapp/static/js/2.09cae44b.chunk.js"
+  },
+  {
+    "revision": "b15b78ed56ee89efcee8578f85669660",
+    "url": "/todoapp/static/js/2.09cae44b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ffb368cd0eea7b418c0",
+    "url": "/todoapp/static/js/main.f3dd1deb.chunk.js"
+  },
+  {
+    "revision": "a0dc61240e341eaac320",
+    "url": "/todoapp/static/js/runtime-main.ec8ab811.js"
+  },
+  {
+    "revision": "37c6455fd06185db8ea46a4289c6ca91",
+    "url": "/todoapp/static/media/success.37c6455f.svg"
+  },
+  {
+    "revision": "81352013ab9ade47e5a45074c4913653",
+    "url": "/todoapp/static/media/tick-sign.81352013.svg"
+  },
+  {
+    "revision": "671482d22869f019e10494ac33867740",
+    "url": "/todoapp/static/media/verified.671482d2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/levanthao1.github.io/TodoList-React/precache-manifest.a71e0264687027c31e0d12958594ca98.js"
+  "/todoapp/precache-manifest.b5389184410b6e327f8190fa0dcb5542.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/levanthao1.github.io/TodoList-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
